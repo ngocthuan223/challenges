cc41b54b43ac0a69535dffb2758263023670c910
dt0801 <94697652+dt0810@users.noreply.github.com>
2022-05-10 16:58:18 +0700

    push sodoku test

--- src/trong/valid_sudoku.js
+++ src/trong/valid_sudoku.js
@@ -1,28 +1,31 @@
 const validSudoku = (sodoku) => {
-    for (let i = 0; i < board.length; i++) { 
-        for (let j = 0; j < board.length; j++) {
-          let cell = board[i][j];
+    for (let i = 0; i < input.length; i++) { 
+        for (let j = 0; j < input.length; j++) {
+          let cell = input[i][j];
           if(cell !== ".") {
-            if (rows[i].includes(cell) {
-              return false
-            } else rows[i].push(cell);
+            if (rows[i].includes(cell)) {
+              return false 
+            } 
+            else rows[i].push(cell);
     
-            if (columns[j].includes(cell) {
+            if (columns[j].includes(cell)) {
               return false;
-            } else columns[j].push(cell);
+            } 
+            else columns[j].push(cell);
     
             let boxIndex = Math.floor((i / 3)) * 3 + Math.floor(j / 3);
     
-            if (boxes[boxIndex].includes(cell) {
+            if (boxes[boxIndex].includes(cell)) {
               return false;
-            } else boxes[boxIndex].push(cell);
+            } 
+            else boxes[boxIndex].push(cell);
     
           }
         }
       } 
       return true;
 }
+
 module.exports = {
     validSudoku
 }
-
